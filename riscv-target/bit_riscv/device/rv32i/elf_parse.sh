8085414f1a7d99d2cdb9a6994a60c3dd9a94a622
bafeigum <bafeigum@gmail.com>
2018-11-19 11:40:29 -0800

    Add bit_riscv target. Add an elf_parse to extract the required data from the ELF file of each test. Currently calls Spike, but the generated files are correct.

---
+++ riscv-target/bit_riscv/device/rv32i/elf_parse.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+riscv64-unknown-elf-objcopy -O binary -j .text.init $1 $1.bin.text
+xxd -e -c 4 $1.bin.text | awk '{print $2}' > $1.mem.text
+
+riscv64-unknown-elf-objcopy -O binary -j .data $1 $1.bin.data
+xxd -e -c 4 $1.bin.data | awk '{print $2}' > $1.mem.data
+
+readelf -S $1 > $1.sections
+grep ".data" $1.sections | awk '{print "0x"$5}' > $1.data.start
+
+readelf -S $1 > $1.sections
+grep ".data" $1.sections | awk '{print "0x"$7}' > $1.data.size
+
+grep "<begin_signature>:" $1.objdump | awk '{print "0x"$1}' > $1.signature.start
+
+declare -i data_start=$(cat $1.data.start)
+declare -i data_size=$(cat $1.data.size)
+declare -i signature_start=$(cat $1.signature.start)
+
+echo $data_start
+echo $data_size
+echo $signature_start
+
+signature_size=$(($data_size-$(($signature_start - $data_start))))
+echo $signature_size > $1.signature.size
+
+
